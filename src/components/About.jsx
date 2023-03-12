4a33302950ca8305a86bb058f31a184a0c3852ff
comaecod <vishnuthecoder@gmail.com>
2023-03-12 11:52:51 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>Introduction</p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] w-auto leading-[30px] text-center'
+      >
+        I am an experienced frontend developer with a strong focus on JavaScript and React, and a keen desire to learn modern technologies. I am committed to delivering high-quality work and collaborating with crossfunctional teams to achieve project goals. I am passionate about frontend development and keen to explore opportunities in React, Next, Remix, and other modern front-end technologies. If you are looking for a collaborative and growth-oriented frontend developer, I would be eager to contribute my expertise to your team.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10 justify-center'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,195 @@
+import {
+  mobile,
+  backend,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  meraai,
+  tcs,
+  meetups,
+  quotes,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'work',
+    title: 'Work',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'React Developer',
+    icon: web,
+  },
+  {
+    title: 'Springboot Developer',
+    icon: mobile,
+  },
+  {
+    title: 'JavaScript Developer',
+    icon: backend,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'MongoDB',
+    icon: mongodb,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Full Stack Developer',
+    company_name: 'Meraai',
+    icon: meraai,
+    iconBg: '#fff',
+    date: 'January 2023 - February 2023',
+    points: [
+      'Worked on developing a PWA using React Vite, Framer, and MUI.',
+      'Worked on Spring Boot and gained an understanding of the CI/CD pipeline.',
+      'Improved development skills and gained modern software insights.',
+    ],
+  },
+  {
+    title: 'Developer & Code Expert',
+    company_name: 'Tata Consultancy Services',
+    icon: tcs,
+    iconBg: '#fff',
+    date: 'February 2020 - December 2022',
+    points: [
+      'Worked on upgrading front-end screens from Struts and JSP to React.js.',
+      'Conducted thorough code reviews, collaborated with the back-end team.',
+      'Developed reusable components for improved code quality, fewer errors, and increased development efficiency.',
+      'This resulted in improved performance, faster rendering, and better user experience.',
+      "Upgrade led to a 30% increase in user productivity, 50% reduction in page load time which significantly improved the app's performance.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
+    name: 'Sara Lee',
+    designation: 'CFO',
+    company: 'Acme Co',
+    image: 'https://randomuser.me/api/portraits/women/4.jpg',
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: 'Chris Brown',
+    designation: 'COO',
+    company: 'DEF Corp',
+    image: 'https://randomuser.me/api/portraits/men/5.jpg',
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: 'Lisa Wang',
+    designation: 'CTO',
+    company: '456 Enterprises',
+    image: 'https://randomuser.me/api/portraits/women/6.jpg',
+  },
+];
+
+const projects = [
+  {
+    name: 'Quotes',
+    description:
+      'This is an SPA that allows users to add quotes and also comment on every quote.',
+    tags: [
+      {
+        name: 'reactjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'firebase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'realtimeDB',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: quotes,
+    source_code_link: 'https://react-http-6e37a.web.app/',
+  },
+  {
+    name: 'Meetups',
+    description:
+      'This is an SPA that allows users to create and list all upcoming meetups.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'vercel',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'mongoDB',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: meetups,
+    source_code_link: 'https://nextjs-meetups-gules.vercel.app/', //'https://github.com/Comaecod/nextjs-meetups',
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
